5b6617671dccdda763bb2e709e2d5b7aafaabeab
nanshen1987 <sn_gis@163.com>
2020-11-14 20:11:57 +0800

    add figsrc

--- code/proc/simple_handler.py
+++ code/proc/simple_handler.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pymc3 as pm
 from theano import tensor as T
+import scipy.io as io
 
+from config.configutil import getpath
 from sim.sim_handler import sim_simple_steps
 
 
@@ -28,25 +30,26 @@ def bayes_single_detector(t, s):
 
 def bayes_multiple_detector(t, s, n):
     with pm.Model() as abrupt_model:
-        sigma = pm.Normal('sigma', mu=10, sigma=5)
+        sigma = pm.Normal('sigma', mu=30, sigma=5)
         # sigma = pm.Uniform('sigma', 5, 15)
         mu = pm.Uniform("mu1", -30, 30)
         tau = pm.DiscreteUniform("tau" + "1", t.min(), t.max())
 
         for i in np.arange(2, n + 2):
-            _mu = pm.Uniform("mu" + str(i), -100, 0)
+            _mu = pm.Uniform("mu" + str(i), -200, 0)
             mu = T.switch(tau >= t, mu, _mu)
             if (i < (n + 1)):
                 tau = pm.DiscreteUniform("tau" + str(i), tau, t.max())
         s_obs = pm.Normal("s_obs", mu=mu, sigma=sigma, observed=s)
 
     with abrupt_model:
-        # pm.find_MAP()
-        trace = pm.sample(5000, tune=2000)
+        pm.find_MAP()
+        trace = pm.sample(20000, tune=5000)
         az.plot_trace(trace)
         plt.show()
         az.plot_autocorr(trace)
         plt.show()
+        az.to_netcdf(trace, getpath('tracepath')+'sim_simple')
         pm.summary(trace)
     return trace
 
@@ -60,8 +63,9 @@ if __name__ == '__main__':
     # plt.show()
     # bayes_single_detector(t, s)
     # 2----bayes_multiple_detector(disp extraction) gauss noise only
-    l, noise, steps = 3600, 10, np.array([[1000, -30], [2500, -30], [3200, 30]])
+    l, noise, steps = 3600, 30, np.array([[1000, -50], [2500, -50], [3200, 100]])
     t, s = sim_simple_steps(l, noise, steps)
+    io.savemat(getpath('data4origin') + 'sim_simple_raw.mat', {'t': t, 'x': s}, oned_as='column')
     plt.plot(t, s)
     plt.show()
     bayes_multiple_detector(t, s, 3)
