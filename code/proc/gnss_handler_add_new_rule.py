1a2926ecb4ad23967c594822b541e5ce29a0e840
nanshen1987 <sn_gis@163.com>
2020-11-19 18:18:49 +0800

    add introduction

--- code/proc/gnss_handler_add_new_rule.py
+++ code/proc/gnss_handler_add_new_rule.py
@@ -14,7 +14,7 @@ from ioext.gnss_sol_handler import loadsol
 def bayes_multiple_detector_each_sigma(t, s, n):
     scala = 1000
     with pm.Model() as abrupt_model:
-        sigma = pm.Normal('sigma1', mu=0.02 * scala, sigma=0.015 * scala)
+        sigma = pm.Normal('sigma', mu=0.02 * scala, sigma=0.015 * scala)
         # sigma = pm.Uniform('sigma', 5, 15)
         mu = pm.Uniform("mu1", -1.5 * scala, -1.4 * scala)
         tau = pm.DiscreteUniform("tau" + "1", t.min(), t.max())
@@ -22,34 +22,22 @@ def bayes_multiple_detector_each_sigma(t, s, n):
         for i in np.arange(2, n + 2):
             _mu = pm.Uniform("mu" + str(i), -1.6 * scala, -1.4 * scala)
             mu = T.switch(tau >= t, mu, _mu)
-            # _sigma = pm.Normal('sigma' + str(i), mu=0.02 * scala, sigma=0.015 * scala)
-            # sigma = T.switch(tau >= t, sigma, _sigma)
             if i < (n + 1):
-                # BoundedUniform = pm.Bound(pm.Uniform, lower=700)
-                # dtau = BoundedUniform('dtau'+str(i), upper=t.max()-tau)
                 ttau = pm.DiscreteUniform("tau" + str(i), tau, t.max())
-
                 tau = ttau
 
-        # add bound
         tau1 = abrupt_model["tau1"]
         tau2 = abrupt_model["tau2"]
         dtau = pm.DiscreteUniform('dtau', tau1 + 500, tau2)
-        # BoundedUniform = pm.Bound(pm.HalfNormal, lower=300)
-        # dtau = BoundedUniform('dtau', mu=(tau2-tau1)/2, sigma=30)
-        # dtau = pm.Pareto("dtau", alpha=1, m=tau2 - tau1)
-        # dtau = pm.Pareto("dtau", alpha=1.0/(tau2 - tau1), m=500)
-        # d = tau2 - tau1
-        # dv = T.switch(d < 700, 0, dtau)
 
         s_obs = pm.Normal("s_obs", mu=mu, sigma=sigma, observed=s)
     g = pm.model_to_graphviz(abrupt_model)
     g.view()
     with abrupt_model:
-        pm.find_MAP()
+        # pm.find_MAP()
         trace = pm.sample(20000, tune=5000)
         az.plot_trace(trace)
-        az.to_netcdf(trace, getpath('tracepath') + 'bd9_4_each_sigma')
+        az.to_netcdf(trace, getpath('tracepath') + 'bd9_4_add_new_rule')
         plt.show()
         pm.summary(trace)
     return trace
