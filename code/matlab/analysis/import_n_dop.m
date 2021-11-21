03749876f2b769701344a198767f8c0fee776a4b
nanshen1987 <sn_gis@163.com>
2021-11-21 15:26:44 +0800

    nsfc

---
+++ code/matlab/analysis/import_n_dop.m
@@ -0,0 +1,54 @@
+function ndop1 = import_n_dop(filename, startRow, endRow)
+%IMPORTFILE 将文本文件中的数值数据作为矩阵导入。
+%   NDOP1 = IMPORTFILE(FILENAME) 读取文本文件 FILENAME 中默认选定范围的数据。
+%
+%   NDOP1 = IMPORTFILE(FILENAME, STARTROW, ENDROW) 读取文本文件 FILENAME 的
+%   STARTROW 行到 ENDROW 行中的数据。
+%
+% Example:
+%   ndop= import_n_dop('n_dop.txt', 2, 13309);
+%
+%    另请参阅 TEXTSCAN。
+
+% 由 MATLAB 自动生成于 2021/09/11 23:36:51
+
+%% 初始化变量。
+if nargin<=2
+    startRow = 2;
+    endRow = inf;
+end
+
+%% 每个文本行的格式:
+%   列1: 双精度值 (%f)
+%	列2: 双精度值 (%f)
+%   列3: 双精度值 (%f)
+%	列4: 双精度值 (%f)
+%   列5: 双精度值 (%f)
+%	列6: 双精度值 (%f)
+%   列7: 双精度值 (%f)
+% 有关详细信息，请参阅 TEXTSCAN 文档。
+formatSpec = '%4f%9f%8f%9f%9f%9f%f%[^\n\r]';
+
+%% 打开文本文件。
+fileID = fopen(filename,'r');
+
+%% 根据格式读取数据列。
+% 该调用基于生成此代码所用的文件的结构。如果其他文件出现错误，请尝试通过导入工具重新生成代码。
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'EmptyValue', NaN, 'HeaderLines', startRow(1)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'EmptyValue', NaN, 'HeaderLines', startRow(block)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% 关闭文本文件。
+fclose(fileID);
+
+%% 对无法导入的数据进行的后处理。
+% 在导入过程中未应用无法导入的数据的规则，因此不包括后处理代码。要生成适用于无法导入的数据的代码，请在文件中选择无法导入的元胞，然后重新生成脚本。
+
+%% 创建输出变量
+ndop1 = table(dataArray{1:end-1}, 'VariableNames', {'week','second','n','gdop','pdop','hdop','vdop'});
+
---
+++ code/matlab/analysis/import_snr_mp.m
@@ -0,0 +1,95 @@
+function snrmp = import_snr_mp(filename, startRow, endRow)
+%IMPORTFILE 将文本文件中的数值数据作为矩阵导入。
+%   SNRMP = IMPORTFILE(FILENAME) 读取文本文件 FILENAME 中默认选定范围的数据。
+%
+%   SNRMP = IMPORTFILE(FILENAME, STARTROW, ENDROW) 读取文本文件 FILENAME 的
+%   STARTROW 行到 ENDROW 行中的数据。
+%
+% Example:
+%   snrmp = importfile('snr_mp.txt', 2, 99549);
+%
+%    另请参阅 TEXTSCAN。
+
+% 由 MATLAB 自动生成于 2021/09/11 21:41:49
+
+%% 初始化变量。
+if nargin<=2
+    startRow = 2;
+    endRow = inf;
+end
+
+%% 将数据列作为文本读取:
+% 有关详细信息，请参阅 TEXTSCAN 文档。
+formatSpec = '%4s%10s%8s%9s%9s%10s%s%[^\n\r]';
+
+%% 打开文本文件。
+fileID = fopen(filename,'r');
+
+%% 根据格式读取数据列。
+% 该调用基于生成此代码所用的文件的结构。如果其他文件出现错误，请尝试通过导入工具重新生成代码。
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'HeaderLines', startRow(1)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'HeaderLines', startRow(block)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% 关闭文本文件。
+fclose(fileID);
+
+%% 将包含数值文本的列内容转换为数值。
+% 将非数值文本替换为 NaN。
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5,6,7]
+    % 将输入元胞数组中的文本转换为数值。已将非数值文本替换为 NaN。
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1)
+        % 创建正则表达式以检测并删除非数值前缀和后缀。
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData(row), regexstr, 'names');
+            numbers = result.numbers;
+            
+            % 在非千位位置中检测到逗号。
+            invalidThousandsSeparator = false;
+            if numbers.contains(',')
+                thousandsRegExp = '^[-/+]*\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'))
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % 将数值文本转换为数值。
+            if ~invalidThousandsSeparator
+                numbers = textscan(char(strrep(numbers, ',', '')), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch
+            raw{row, col} = rawData{row};
+        end
+    end
+end
+
+
+%% 将非数值元胞替换为 NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % 查找非数值元胞
+raw(R) = {NaN}; % 替换非数值元胞
+
+%% 创建输出变量
+snrmp = table;
+snrmp.week = cell2mat(raw(:, 1));
+snrmp.second = cell2mat(raw(:, 2));
+snrmp.prn = cell2mat(raw(:, 3));
+snrmp.az = cell2mat(raw(:, 4));
+snrmp.ele = cell2mat(raw(:, 5));
+snrmp.snr = cell2mat(raw(:, 6));
+snrmp.mp = cell2mat(raw(:, 7));
+
